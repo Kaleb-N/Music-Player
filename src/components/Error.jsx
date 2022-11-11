1825e113670cdfe83c69f7682806b2aa78621770
PC <nwachukwukaleb@gmail.com>
2022-11-11 16:01:02 +0100

    updated

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-
 const Error = () => (
-  <div>Error</div>
+  <div className="w-full flex justify-center items-center flex-col">
+    <img src={loader} alt="loader" className="w-32 h-32" />
+    <h1 className="font-bold text-2xl text-white mt-2">Something went wrong, please try again.</h1>
+  </div>
 );
 
 export default Error;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,39 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', 'd6885e8c21msh13d012ed4184206p17b144jsn4132cd6140c4');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  })
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
+
+
+
+
+
+
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': 'd6885e8c21msh13d012ed4184206p17b144jsn4132cd6140c4',
+//     'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+//   }
+// };
+  
+// fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
