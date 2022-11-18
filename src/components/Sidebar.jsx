bb64b0a7476b84123d382c80aa6846236d7ff213
PC <nwachukwukaleb@gmail.com>
2022-11-18 17:29:21 +0100

    updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -35,8 +35,8 @@ const Sidebar = () => {
       {/* mobile menu */}
       <div className="absolute md:hidden block top-6 right-3">
         {mobileMenuOpen ? (
-          <RiCloseLine className="w-6 h-6 text-white mr-3 cursor-pointer" onClick={() => setMobileMenuOpen(false)} />
-        ) : <HiOutlineMenu className="w-6 h-6 text-white mr-3 cursor-pointer" onClick={() => setMobileMenuOpen(true)} />}
+          <RiCloseLine className="w-6 h-6 text-white mr-2 cursor-pointer" onClick={() => setMobileMenuOpen(false)} />
+        ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2 cursor-pointer" onClick={() => setMobileMenuOpen(true)} />}
       </div>
 
       <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
